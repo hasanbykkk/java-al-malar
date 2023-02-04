e3ef74a1b02c75244d371f0a04cca4a33d6853d8
hasanbykkk <99807200+hasanbykkk@users.noreply.github.com>
2023-02-04 16:30:24 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,75 +1,94 @@
-import java.util.Scanner;
 
 
+import com.sun.jdi.event.MonitorWaitedEvent;
 
+import java.awt.desktop.ScreenSleepEvent;
+import java.security.KeyStore;
+import java.util.*;
+import java.util.Random;
 
 public class Main {
-    static void toplama(){
-        Scanner scanner= new Scanner(System.in);
-        System.out.println("birinci sayıyı giriniz");
-        int a=scanner.nextInt();
-        System.out.println("ikinci sayıyı giriniz");
-        int b= scanner.nextInt();
-        System.out.println("sayıların toplamı"+""+(a+b));
+
+    static void ekle(List liste){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("eklenecek kişi");
+        String kişi = scanner.nextLine();
+        liste.add(kişi);
+
+    }
+    static void sil(List liste){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("silinecek kişi");
+        String sil = scanner.nextLine();
+        liste.remove(sil);
+
+    }
+    static void gör(List liste) {
+        int say=0;
+        System.out.println("--------");
+        say++;
+        for (int b = 0; b <liste.size();b++) {
+
+            System.out.println("çekilişteki kişiler"+""+"\n"+liste);
+
+            System.out.println("--------");
+        }
+    }
+    static void seç(List liste){
+        Scanner scanner = new Scanner(System.in);
+        Random rastgele = new Random();
+        int b = rastgele.nextInt();
+        System.out.println("Rastgele seçilen isim: " + liste.get(b));
+
     }
 
-    static void çıkarma(){
-        Scanner scanner= new Scanner(System.in);
-        System.out.println("birinci sayıyı giriniz");
-        int a=scanner.nextInt();
-        System.out.println("ikinci sayıyı giriniz");
-        int b= scanner.nextInt();
-        System.out.println("birinci sayının  ikinci sayıdan çıkması"+""+(a-b));
-    }
-    static void çarpma(){
-        Scanner scanner= new Scanner(System.in);
-        System.out.println("birinci sayıyı giriniz");
-        int a=scanner.nextInt();
-        System.out.println("ikinci sayıyı giriniz");
-        int b= scanner.nextInt();
-        System.out.println("sayıların çarpımı"+""+(a*b));
-    }
-    static void bölme(){
-        Scanner scanner= new Scanner(System.in);
-        System.out.println("birinci sayıyı giriniz");
-        int a=scanner.nextInt();
-        System.out.println("ikinci sayıyı giriniz");
-        int b= scanner.nextInt();
-        System.out.println("sayıların çarpımı"+""+(a/b));
-    }
+    public static void main(String[] args){
+            Scanner scanner = new Scanner(System.in);
+            List liste = new ArrayList();
 
-    public static void main(String[] args) {
-        Scanner scanner= new Scanner(System.in);
+            while (true) {
 
-        while (true) {
-            System.out.println("yapmak istedğiniz işlem \n1-toplama \n2-çıkarma \n3-çarpma\n4-bölme işlemi \n5-çıkış yapmak için herhangi bir tuşa basın");
-            int seç=scanner.nextInt();
+                System.out.println("yapmak istediğiniz işlemi seçiniz?1-kişi ekle 2-kişisil 3-kişileri gör 4-çekiliş yap 5-çıkış");
+                int seç = scanner.nextInt();
 
-            if (seç==1){
-                toplama();
+                if (seç == 1) {
+                    System.out.println("kaç kişi eklenecek?");
+                    int c=scanner.nextInt();
+
+                    for (int a = 0; a < c; a++) {
+
+                        ekle(liste);
+                        if (a == c) {
+                            break;
+                        }
+                    }
+
+                }
+
+
+                else if (seç == 2) {
+                    sil(liste);
+                }
+                else if (seç == 3) {
+
+                    gör(liste);
+
+                }
+                else if (seç == 4) {
+                    seç(liste);
+
+                }
+                else {
+                    System.out.println("çıkış yapılıyor...");
+                    break;
+                }
+
             }
-            else if (seç==2){
-                çıkarma();
-            }
-
-            else if (seç==3){
-                çarpma();
-            }
-            else if (seç==4){
-                bölme();
-            }
-            else {
-
-                System.out.println("çıkış yapılıyor");
-                break;
-            }
-
 
 
         }
 
 
-
     }
-}
+
 
